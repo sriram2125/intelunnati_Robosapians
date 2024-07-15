0613da3844c5cef388aebbd27b422cd414a81615
sriram2125 <sriram.rbb2125@saintgits.org>
2024-07-15 16:03:00 +0530

    Add files via upload

---
+++ setup (1).py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'my_image_listener'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='richu',
+    maintainer_email='richu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'image_listener = my_image_listener.image_listener:main'
+        ],
+    },
+)
